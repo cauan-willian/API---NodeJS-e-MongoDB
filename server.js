2def71e62cb5d70cc83a6ff3f8c2e18bbd545e05
Cauan Willian <148123511+cauan-willian@users.noreply.github.com>
2024-05-27 15:38:32 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+
+const app = express()
+app.use(express.json())
+const users = []
+
+app.post('/usuarios', async (req, res) => {
+
+    await prisma.user.create ({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+      
+
+    res.status(201).json(req.body)
+})
+
+app.get('/usuarios', async (req, res) => {
+    
+        const user = await prisma.user.findMany()
+
+        res.status(200).json(users)
+})
+
+
+app.listen(3000)
+
+/*  
+    Criação API
+    cauanwillian11    HLabt3722LmEQp2i
+*/
+
+
+
+
+
+
+
+
